2f419abc8c82ba263c79f01c8efff341c9225bd3
= <=>
2025-02-26 12:21:43 +0000

    Fix commit refactor

--- app/src/main/java/com/example/madwork/MainActivity.kt
+++ app/src/main/java/com/example/madwork/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
@@ -27,6 +28,7 @@ import org.ramani.compose.MapLibre
 class MainActivity : ComponentActivity(), LocationListener {
     private val locationViewModel: LocationViewModel by viewModels()
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         checkPermissions()
